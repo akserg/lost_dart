80abed7617558c200530ae014133e601a6fe0da5
Sergey Akopkokhyants <akserg@gmail.com>
2013-07-02 10:30:42 +0200

    Library design has been changed and simplified

--- example/server/example.dart
+++ example/server/example.dart
@@ -1,49 +1,54 @@
+//Copyright (C) 2012-2013 Sergey Akopkokhyants. All Rights Reserved.
+//Author: akserg
+
+library example;
+
 import 'package:lost_dart/lost_dart.dart';
 
-//application code
-class Network{
-  String uri;
-  
-  Network(this.uri);
-}
-
-class User {
-  String name;
-}
-
-class Manager{
-  Network network;
-  User user;
-
-  Manager(this.user);
-}
-
 void main() {
-  // Create IoC container
+  // Create container
   Container container = new Container();
+  
+  // Bind Baz
+  container.bind(Baz).addConstructorConstArg("story");
+  // Bind Bar
+  container.bind(Bar).addConstructorTypeArg(Baz);
+  // Bind Foo
+  container.bind(Foo).setTypeProperty("bar", Bar);
+  
+  // Resolve baz
+  Baz baz = container.get(Baz);
+  print(baz);
+  Foo foo = container.get(Foo);
+  print(foo);
+}
 
-  // Add host
-  container.bindAs("host").toFactory((){
-    return "http://127.0.0.1";
-  });
-  
-  // Add Network
-  container.bind(Network).addConstructorRefArg("host");
-  
-  // Add User
-  container.bind(User).setConstProperty("name", "Admin");
-  
-  // Add Manager
-  container.bind(Manager).toFactory((){
-    Manager bar = new Manager(container.get(User));
-    bar.network = container.get(Network);
-    return bar;
-  });
-  
-  // Resolve Manager
-  Manager manager = container.get(Manager);
+// Somewhere in developing library
+class Baz {
+  String name;
 
-  // Test result
-  assert(manager.network.uri == "http://127.0.0.1");
-  assert(manager.user.name == "Admin");
+  Baz([this.name = ""]);
+}
+
+class Bar {
+  Baz baz;
+  Bar(this.baz);
+}
+
+class Foo {
+  Bar bar;
+}
+
+abstract class Weapon {
+  String name;
+  
+  Weapon(this.name);
+}
+
+class Gun extends Weapon {
+  Gun() : super("Gun");
+}
+
+class MachineGun extends Weapon {
+  MachineGun() : super("Machine Gun");
 }
